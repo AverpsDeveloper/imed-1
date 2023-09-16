78b768fd6af03a13453e0979c4f9c92a8703448f
Mady <mady@averps.com>
2023-09-16 12:06:56 +0530

    modified  Addcategory in inventory section

--- project-imed/components/inventory/AddCategory.jsx
+++ project-imed/components/inventory/AddCategory.jsx
@@ -56,7 +56,7 @@ function AddCategory() {
   );
 }
 
-export default AddCategory;*/}
+export default AddCategory;
 
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
@@ -70,7 +70,7 @@ function AddCategoryForm() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+     <div className="min-h-screen flex items-center justify-center"> 
       <div className="bg-white w-full max-w-md rounded-lg shadow-md p-6">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Add Category</h1>
@@ -139,5 +139,87 @@ function AddCategoryForm() {
 }
 
 export default AddCategoryForm;
+*/}
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
+function AddCategoryForm() {
+  const { handleSubmit, control } = useForm();
 
+  const onSubmit = (data) => {
+    // Handle the form submission here
+    console.log(data);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-200 flex flex-col ">
+      <div className="bg-white w-full h-full p-6">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-bold">Add Category</h1>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Category List
+          </button>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label className="block text-gray-600">Category Name</label>
+            <Controller
+              name="categoryName"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type="text"
+                  className="border rounded w-full px-3 py-2 mt-1"
+                  placeholder="Enter category name"
+                />
+              )}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-600">Status</label>
+            <Controller
+              name="categoryStatus"
+              control={control}
+              defaultValue="active"
+              render={({ field }) => (
+                <div className="flex items-center">
+                  <input
+                    {...field}
+                    type="radio"
+                    value="active"
+                    id="active"
+                    className="mr-2"
+                  />
+                  <label htmlFor="active">Active</label>
+                  <input
+                    {...field}
+                    type="radio"
+                    value="inactive"
+                    id="inactive"
+                    className="ml-4 mr-2"
+                  />
+                  <label htmlFor="inactive">Inactive</label>
+                </div>
+              )}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AddCategoryForm;
+
+ 
