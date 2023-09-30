3470d65d16d122bf5940f49e1301b754c98d1d84
jordanaverps <142835883+jordanaverps@users.noreply.github.com>
2023-09-30 18:18:38 +0530

    update forms and validations

---
+++ project-imed/app/dashboard/inventory/items-type/add/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddItemType from '@/components/inventory/AddItemType'
+
+const Inventory = () => {
+  return (
+    <div>
+      <AddItemType/>
+    </div>
+  )
+}
+
+export default Inventory
